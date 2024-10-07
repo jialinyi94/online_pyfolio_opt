a31356c066927e68558a367881926bfd202005fe
Jialin Yi <jialinyi@sas.upenn.edu>
2024-10-07 22:10:01 +0100

    Refactor optimizer.py to use optax._src.base.ScalarOrSchedule for learning rate in mirror_descent and egd functions

--- pyfoliopt/loss.py
+++ pyfoliopt/loss.py
@@ -2,5 +2,5 @@ import chex
 import jax.numpy as jnp
 
 
-def neg_log_dot(relative_prices: chex.Array, portfolio: chex.Array):
+def neg_logdot_loss(relative_prices: chex.Array, portfolio: chex.Array):
     return -jnp.log(jnp.dot(relative_prices, portfolio))
