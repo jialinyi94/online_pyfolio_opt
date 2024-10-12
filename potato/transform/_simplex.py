dd89943f77867ac94952879fa41e92961be7ec1a
Jialin Yi <jialinyi@sas.upenn.edu>
2024-10-12 13:54:33 +0100

    Refactor fix_share function to improve readability and performance

--- potato/transform/_simplex.py
+++ potato/transform/_simplex.py
@@ -1,11 +1,7 @@
-import jax
 import jax.numpy as jnp
 import chex
 
 
-def fix_share(params, eta):
-    def _fix_share(_params: chex.Array):
-        uniform_weights = jnp.ones(shape=_params) / _params.shape[0]
-        return (1 - eta) * _params + eta * uniform_weights
-    
-    return jax.tree_map(_fix_share, params)
+def fix_share(params: chex.Array, eta):
+    uniform_weights = jnp.ones(shape=params) / params.shape[0]
+    return (1 - eta) * params + eta * uniform_weights
